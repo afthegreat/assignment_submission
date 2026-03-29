07a1ac1a8b2dc4bc54f401805a0803f8f1896d02
afthegreat <abelferede916@gmail.com>
2026-03-29 20:41:07 +0300

    Initial Commit

---
+++ src/main/java/com/codecampus/AssignmentSubmissionApp/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.codecampus.AssignmentSubmissionApp.config;
+
+public class SecurityConfig {
+
+}
---
+++ src/main/java/com/codecampus/AssignmentSubmissionApp/service/UserDetailsServiceImpl.java
@@ -0,0 +1,5 @@
+package com.codecampus.AssignmentSubmissionApp.service;
+
+public class UserDetailsServiceImpl {
+
+}
