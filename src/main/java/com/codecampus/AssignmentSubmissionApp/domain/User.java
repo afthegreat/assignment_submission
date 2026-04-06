7cbc066a73195433189cd38fcb29185b0c0e09d2
afthegreat <abelferede916@gmail.com>
2026-04-06 23:36:15 +0300

    first login trial with bcrypt done

--- src/main/java/com/codecampus/AssignmentSubmissionApp/domain/User.java
+++ src/main/java/com/codecampus/AssignmentSubmissionApp/domain/User.java
@@ -1,6 +1,12 @@
 package com.codecampus.AssignmentSubmissionApp.domain;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,8 +16,9 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name="users")
-public class User {
-    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+public class User implements UserDetails{
+    private static final long serialVersionUID = -443143418636020766L;
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
     private LocalDate cohortStartDate;
 	private String username;
@@ -22,17 +29,49 @@ public class User {
 	public void setId(long id) {
 		this.id = id;
 	}
+	@Override
 	public String getUsername() {
 		return username;
 	}
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	@Override
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+	    return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+	    return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+	    return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+	    return true;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+    List<GrantedAuthority> roles= new ArrayList<>();
+    roles.add(new Authority("Role_STUDENT") {
+	@Override
+	public String getAuthority() {
+		return "Role_Student";
+	}
+});
+return roles;
 	}
 	
 	
--- src/main/java/com/codecampus/AssignmentSubmissionApp/service/UserDetailsServiceImpl.java
+++ src/main/java/com/codecampus/AssignmentSubmissionApp/service/UserDetailsServiceImpl.java
@@ -1,5 +1,26 @@
 package com.codecampus.AssignmentSubmissionApp.service;
 
-public class UserDetailsServiceImpl {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.codecampus.AssignmentSubmissionApp.domain.User;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		User user=new User();
+		user.setUsername(username);
+		user.setPassword("asdfasdf");
+	    user.setPassword(new BCryptPasswordEncoder().encode("asdfasdf"));
+		user.setId(1);
+		return user;
+		
+	}
 
 }
